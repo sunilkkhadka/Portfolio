54eeeca80d8e8c2c073944e4bbb4e9af81c0328f
Sunil Khadka <sunil.khadka001@gmail.com>
2024-09-22 21:24:32 +0545

    T5: Added contact and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./assets/styles/main.scss";
-import Experience from "./components/Experience";
 import Hero from "./components/Hero";
+import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import TechStack from "./components/TechStack";
+import Experience from "./components/Experience";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <TechStack />
       <Experience />
       <Projects />
+      <Contact />
     </main>
   );
 }
