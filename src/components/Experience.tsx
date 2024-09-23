2699ad4e0c14ef8edd309b2fe87d6abeb9dcaf6b
Sunil Khadka <sunil.khadka001@gmail.com>
2024-09-23 15:00:33 +0545

    T8: Added scroll to specific section functionality

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,3 +1,5 @@
+import { Element } from "react-scroll";
+
 import CssIcon from "../assets/svg/css.svg";
 import HtmlIcon from "../assets/svg/html.svg";
 import SassIcon from "../assets/svg/sass.svg";
@@ -16,146 +18,151 @@ import ReactQueryIcon from "../assets/svg/react-query.svg";
 
 const Experience = () => {
   return (
-    <section className="experiences">
-      <div className="wrapper experiences__container">
-        <div className="experiences__intro">
-          <h1 className="experiences__title">My Experiences</h1>
-          <p className="experiences__description">
-            A journey of continuous learning and development, where each role
-            has sharpened my skills and deepened my passion for crafting
-            efficient, scalable solutions.
-          </p>
-        </div>
-        <div className="experiences__list">
-          <div className="experiences__item">
-            <div className="experiences__info">
-              <img
-                src={PointIcon}
-                className="experiences__bullet-point"
-                alt="Point"
-              />
-              <div className="experiences__date">Jul 2022 - Apr 2024</div>
-              <div className="experiences__company-name">Software Engineer</div>
-              <div className="experiences__position">
-                Machnet Technologies Inc.
+    <Element name="experiences">
+      <section className="experiences">
+        <div className="wrapper experiences__container">
+          <div className="experiences__intro">
+            <h1 className="experiences__title">My Experiences</h1>
+            <p className="experiences__description">
+              A journey of continuous learning and development, where each role
+              has sharpened my skills and deepened my passion for crafting
+              efficient, scalable solutions.
+            </p>
+          </div>
+          <div className="experiences__list">
+            <div className="experiences__item">
+              <div className="experiences__info">
+                <img
+                  src={PointIcon}
+                  className="experiences__bullet-point"
+                  alt="Point"
+                />
+                <div className="experiences__date">Jul 2022 - Apr 2024</div>
+                <div className="experiences__company-name">
+                  Software Engineer
+                </div>
+                <div className="experiences__position">
+                  Machnet Technologies Inc.
+                </div>
+                <div className="experiences__work-description">
+                  Developed user interfaces for management dashboards and client
+                  onboarding using React.js and TypeScript. Boosted performance
+                  by integrating RESTful APIs with React Query, reducing API
+                  calls. Contributed to planning and documenting client
+                  features, ensuring clear requirements and efficient
+                  development.
+                </div>
+                <div className="experiences__tech-badges">
+                  <p>Tools used:</p>
+                  <img
+                    src={ReactIcon}
+                    className="experiences__tech-badge"
+                    alt="React js"
+                  />
+                  <img
+                    src={GolangIcon}
+                    className="experiences__tech-badge"
+                    alt="Golang"
+                  />
+                  <img
+                    src={MysqlIcon}
+                    className="experiences__tech-badge"
+                    alt="MySQL"
+                  />
+                  <img
+                    src={ReactQueryIcon}
+                    className="experiences__tech-badge"
+                    alt="React Query"
+                  />
+                  <img
+                    src={JavascriptIcon}
+                    className="experiences__tech-badge"
+                    alt="JavaScript"
+                  />
+                  <img
+                    src={TypescriptIcon}
+                    className="experiences__tech-badge"
+                    alt="TypeScript"
+                  />
+                  <img
+                    src={SassIcon}
+                    className="experiences__tech-badge"
+                    alt="Sass"
+                  />
+                  <img
+                    src={DockerIcon}
+                    className="experiences__tech-badge"
+                    alt="Docker"
+                  />
+                  <img
+                    src={JenkinsIcon}
+                    className="experiences__tech-badge"
+                    alt="Jenkins"
+                  />
+                  <img
+                    src={PostmanIcon}
+                    className="experiences__tech-badge"
+                    alt="Postman"
+                  />
+                </div>
               </div>
-              <div className="experiences__work-description">
-                Developed user interfaces for management dashboards and client
-                onboarding using React.js and TypeScript. Boosted performance by
-                integrating RESTful APIs with React Query, reducing API calls.
-                Contributed to planning and documenting client features,
-                ensuring clear requirements and efficient development.
-              </div>
-              <div className="experiences__tech-badges">
-                <p>Tools used:</p>
+            </div>
+            <div className="experiences__item">
+              <div className="experiences__info">
                 <img
-                  src={ReactIcon}
-                  className="experiences__tech-badge"
-                  alt="React js"
+                  src={PointIcon}
+                  className="experiences__bullet-point"
+                  alt="Point"
                 />
-                <img
-                  src={GolangIcon}
-                  className="experiences__tech-badge"
-                  alt="Golang"
-                />
-                <img
-                  src={MysqlIcon}
-                  className="experiences__tech-badge"
-                  alt="MySQL"
-                />
-                <img
-                  src={ReactQueryIcon}
-                  className="experiences__tech-badge"
-                  alt="React Query"
-                />
-                <img
-                  src={JavascriptIcon}
-                  className="experiences__tech-badge"
-                  alt="JavaScript"
-                />
-                <img
-                  src={TypescriptIcon}
-                  className="experiences__tech-badge"
-                  alt="TypeScript"
-                />
-                <img
-                  src={SassIcon}
-                  className="experiences__tech-badge"
-                  alt="Sass"
-                />
-                <img
-                  src={DockerIcon}
-                  className="experiences__tech-badge"
-                  alt="Docker"
-                />
-                <img
-                  src={JenkinsIcon}
-                  className="experiences__tech-badge"
-                  alt="Jenkins"
-                />
-                <img
-                  src={PostmanIcon}
-                  className="experiences__tech-badge"
-                  alt="Postman"
-                />
+                <div className="experiences__date">Apr 2022 - Jul 2022</div>
+                <div className="experiences__company-name">
+                  Software Engineer Trainee
+                </div>
+                <div className="experiences__position">
+                  Machnet Technologies Inc.
+                </div>
+                <div className="experiences__work-description">
+                  Developed static web pages using HTML, CSS, SCSS, and
+                  JavaScript. Implemented responsive designs with media queries,
+                  ensuring adaptability across all screen sizes. Optimized
+                  loading speed by 70% through image compression, format
+                  conversion, and lazy-loading. Integrated Google Analytics via
+                  Tag Manager to track traffic and user activity.
+                </div>
+                <div className="experiences__tech-badges">
+                  <p>Tools used:</p>
+                  <img
+                    src={HtmlIcon}
+                    className="experiences__tech-badge"
+                    alt="HTML"
+                  />
+                  <img
+                    src={CssIcon}
+                    className="experiences__tech-badge"
+                    alt="CSS"
+                  />
+                  <img
+                    src={JavascriptIcon}
+                    className="experiences__tech-badge"
+                    alt="JavaScript"
+                  />
+                  <img
+                    src={TypescriptIcon}
+                    className="experiences__tech-badge"
+                    alt="TypeScript"
+                  />
+                  <img
+                    src={SassIcon}
+                    className="experiences__tech-badge"
+                    alt="Sass"
+                  />
+                </div>
               </div>
             </div>
           </div>
-          <div className="experiences__item">
-            <div className="experiences__info">
-              <img
-                src={PointIcon}
-                className="experiences__bullet-point"
-                alt="Point"
-              />
-              <div className="experiences__date">Apr 2022 - Jul 2022</div>
-              <div className="experiences__company-name">
-                Software Engineer Trainee
-              </div>
-              <div className="experiences__position">
-                Machnet Technologies Inc.
-              </div>
-              <div className="experiences__work-description">
-                Developed static web pages using HTML, CSS, SCSS, and
-                JavaScript. Implemented responsive designs with media queries,
-                ensuring adaptability across all screen sizes. Optimized loading
-                speed by 70% through image compression, format conversion, and
-                lazy-loading. Integrated Google Analytics via Tag Manager to
-                track traffic and user activity.
-              </div>
-              <div className="experiences__tech-badges">
-                <p>Tools used:</p>
-                <img
-                  src={HtmlIcon}
-                  className="experiences__tech-badge"
-                  alt="HTML"
-                />
-                <img
-                  src={CssIcon}
-                  className="experiences__tech-badge"
-                  alt="CSS"
-                />
-                <img
-                  src={JavascriptIcon}
-                  className="experiences__tech-badge"
-                  alt="JavaScript"
-                />
-                <img
-                  src={TypescriptIcon}
-                  className="experiences__tech-badge"
-                  alt="TypeScript"
-                />
-                <img
-                  src={SassIcon}
-                  className="experiences__tech-badge"
-                  alt="Sass"
-                />
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Element>
   );
 };
 
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -12,45 +12,48 @@ import PostmanIcon from "../assets/svg/postman.svg";
 import AWSIcon from "../assets/svg/aws.svg";
 import ReduxIcon from "../assets/svg/redux.svg";
 import LinuxIcon from "../assets/svg/linux.svg";
+import { Element } from "react-scroll";
 
 const TechStack = () => {
   return (
-    <section className="techstack">
-      <div className="wrapper techstack__container">
-        <div className="techstack__images">
-          <img src={ReactIcon} className="techstack__icon" alt="React js" />
-          <img src={GolangIcon} className="techstack__icon" alt="Golang" />
-          <img
-            src={TypescriptIcon}
-            className="techstack__icon"
-            alt="TypeScript"
-          />
-          <img src={SassIcon} className="techstack__icon" alt="Sass" />
-          <img src={MysqlIcon} className="techstack__icon" alt="MySQL" />
-          <img src={ReduxIcon} className="techstack__icon" alt="Redux" />
-          <img src={HtmlIcon} className="techstack__icon" alt="HTML" />
-          <img src={CssIcon} className="techstack__icon" alt="CSS" />
-          <img
-            src={JavascriptIcon}
-            className="techstack__icon"
-            alt="JavaScript"
-          />
-          <img src={DockerIcon} className="techstack__icon" alt="Docker" />
-          <img src={JenkinsIcon} className="techstack__icon" alt="Jenkins" />
-          <img src={PostmanIcon} className="techstack__icon" alt="Postman" />
-          <img src={LinuxIcon} className="techstack__icon" alt="Linux" />
-          <img src={AWSIcon} className="techstack__icon" alt="AWS" />
+    <Element name="techstack">
+      <section className="techstack">
+        <div className="wrapper techstack__container">
+          <div className="techstack__images">
+            <img src={ReactIcon} className="techstack__icon" alt="React js" />
+            <img src={GolangIcon} className="techstack__icon" alt="Golang" />
+            <img
+              src={TypescriptIcon}
+              className="techstack__icon"
+              alt="TypeScript"
+            />
+            <img src={SassIcon} className="techstack__icon" alt="Sass" />
+            <img src={MysqlIcon} className="techstack__icon" alt="MySQL" />
+            <img src={ReduxIcon} className="techstack__icon" alt="Redux" />
+            <img src={HtmlIcon} className="techstack__icon" alt="HTML" />
+            <img src={CssIcon} className="techstack__icon" alt="CSS" />
+            <img
+              src={JavascriptIcon}
+              className="techstack__icon"
+              alt="JavaScript"
+            />
+            <img src={DockerIcon} className="techstack__icon" alt="Docker" />
+            <img src={JenkinsIcon} className="techstack__icon" alt="Jenkins" />
+            <img src={PostmanIcon} className="techstack__icon" alt="Postman" />
+            <img src={LinuxIcon} className="techstack__icon" alt="Linux" />
+            <img src={AWSIcon} className="techstack__icon" alt="AWS" />
+          </div>
+          <div className="techstack__intro">
+            <h1 className="techstack__title">My Tech Stack</h1>
+            <p className="techstack__description">
+              My skill set includes a variety of programming languages and
+              frameworks, ensuring I can tackle front-end, back-end, and full
+              stack development.
+            </p>
+          </div>
         </div>
-        <div className="techstack__intro">
-          <h1 className="techstack__title">My Tech Stack</h1>
-          <p className="techstack__description">
-            My skill set includes a variety of programming languages and
-            frameworks, ensuring I can tackle front-end, back-end, and full
-            stack development.
-          </p>
-        </div>
-      </div>
-    </section>
+      </section>
+    </Element>
   );
 };
 
